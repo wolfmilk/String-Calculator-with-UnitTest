8d76c4f56d9bc4e254a06ff74e1a4cda0f90cce5
Madani Djallil <mdjall1@live.spcollege.edu>
2018-12-05 15:29:34 -0500

    Initial commit

---
+++ UnitTestProject1/StringCalculator.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject1
+{
+        public class StringCalculator
+        {
+            public int Add(string input)
+            {
+                if (HasCustomDelimiter(input))
+                {
+                    string delimiters = StripOffNumbers(input);
+                    input = StripOffDelimiterMetadata(input);
+                    return AddNumbers(input, GetDelimiters(delimiters));
+                }
+
+                return AddNumbers(input, GetDelimiters(string.Empty));
+            }
+
+            private string StripOffNumbers(string input)
+            {
+                return input.Substring(0, input.IndexOf('\n')).Substring(2);
+            }
+
+            private int AddNumbers(string input, string[] delimiters)
+            {
+                if (input.Length == 0)
+                {
+                    return 0;
+                }
+
+                return SplitNumbers(input, delimiters).Select(ParseNumber).Where(n => n < 1000).Sum();
+            }
+
+            private static int ParseNumber(string s)
+            {
+                int result = int.Parse(s);
+                if (result < 0)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+
+                return result;
+            }
+
+            private string[] SplitNumbers(string input, string[] delimiters)
+            {
+                return input.Split(delimiters, StringSplitOptions.None);
+            }
+
+            private string StripOffDelimiterMetadata(string input)
+            {
+                if (HasCustomDelimiter(input))
+                {
+                    return input.Substring(input.IndexOf('\n') + 1);
+                }
+
+                return input;
+            }
+
+            private static bool HasCustomDelimiter(string input)
+            {
+                return input.StartsWith("//");
+            }
+
+            private string[] GetDelimiters(string customDelimiters)
+            {
+                var delimiters = new List<string> { ",", "\n" };
+                if (string.IsNullOrEmpty(customDelimiters))
+                {
+                    return delimiters.ToArray();
+                }
+
+                if (customDelimiters.StartsWith("["))
+                {
+                    var longDelimiters = customDelimiters.Replace("[", "]").Split(new[] { ']' }, StringSplitOptions.RemoveEmptyEntries);
+                    delimiters.AddRange(longDelimiters);
+                    return delimiters.ToArray();
+                }
+
+                delimiters.Add(customDelimiters[0].ToString());
+                return delimiters.ToArray();
+            }
+
+        }
+    }
+
---
+++ UnitTestProject2/StringCalculatorTest.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTestProject1;
+
+namespace UnitTestProject2
+{
+    [TestClass]
+    public class StringCalculatorTest
+    {
+        [TestMethod]
+        public void Add_EmptyString_ReturnsZero()
+        {
+          StringCalculator calculator= new StringCalculator();
+          var result = calculator.Add("");
+
+            Assert.AreEqual(0, result);
+        }
+
+       
+
+        [TestMethod]
+        public void Add_SingleNumber_ReturnsNumber()
+        {
+            StringCalculator calculator = new StringCalculator();
+            var result = calculator.Add("1");
+
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void Add_TwoNumbers_ReturnsSum()
+        {
+            StringCalculator calculator = new StringCalculator();
+            var result = calculator.Add("12,4");
+
+            Assert.AreEqual(16, result);
+        }
+
+        [TestMethod]
+        public void Add_MultipleNumbers_ReturnsSum()
+
+        {
+            StringCalculator calculator = new StringCalculator();
+            var result = calculator.Add("1,2,3");
+
+            Assert.AreEqual(6, result);
+           
+        }
+
+       
+        
+
+        [TestMethod, ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Add_NegativeValue_ThrowsException()
+
+        {
+            StringCalculator calculator = new StringCalculator();
+            var result = calculator.Add("-1");
+
+            
+        }
+
+        [TestMethod]
+        public void Add_NumbersLargerThan1000_ReturnsSum()
+        {
+            StringCalculator calculator = new StringCalculator();
+            var result = calculator.Add("2,1001");
+
+            Assert.AreEqual(2, result);
+            
+
+    
+        }
+
+        
+
+       
+       
+    }
+}
